ef1fdd65b979dad61e8cdf82815725e4ba9cd714
infinith4 <infinith4@gmail.com>
2017-11-04 20:01:12 +0900

    add SignalR Sample

---
+++ SignalRWeb.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SignalRWeb.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SignalRWeb/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRWeb
+{
+    public class MyHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void SendMessage(string message)
+        {
+            Clients.All.recieveMessage(message);
+        }
+    }
+}
